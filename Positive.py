c4cd4f094ffe9ad5318989b09eca5c1bf996d665
DeepthiLakkamraju <42571284+DeepthiLakkamraju@users.noreply.github.com>
2018-08-27 14:36:29 +0530

    Update and rename positive.py to Positive.py

---
+++ Positive.py
@@ -0,0 +1,7 @@
+i=int(raw_input())
+if i<0:
+	print "Negative"
+elif i>0:
+	print "Positive"
+else:
+	print "Zero"
--- positive.py
+++
@@ -1,7 +0,0 @@
-i=10
-if i<0:
-	print "it is negatve number"
-elif i>0:
-	print"it is positive number"
-else:
-	print"it is equal to zero"
