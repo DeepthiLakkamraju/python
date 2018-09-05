f1566051451abcca75da01b9df59d6094e4683fb
DeepthiLakkamraju <42571284+DeepthiLakkamraju@users.noreply.github.com>
2018-09-05 10:58:44 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,9 @@
+def main():
+    count = 0
+    number = int(raw_input())
+    while (number > 0):
+    	number=number//10
+    	count = count + 1
+    print (count)
+if __name__ == '__main__':
+    main()
